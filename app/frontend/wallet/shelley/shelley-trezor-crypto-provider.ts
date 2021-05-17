e5123726f9cee6834ea0b041fcc39830cba762ff
xdzurman <kamil.dzurman@gmail.com>
2021-05-17 14:35:38 +0200

    Correctly serialize mnemonic voting registration

--- app/frontend/wallet/shelley/shelley-trezor-crypto-provider.ts
+++ app/frontend/wallet/shelley/shelley-trezor-crypto-provider.ts
@@ -309,7 +309,7 @@ const ShelleyTrezorCryptoProvider = async ({
   }
 
   // TODO: export type from trezor if possible
-  const formatAuxiliaryAdata = (txAuxiliaryData: TxAuxiliaryData): any => {
+  const formatAuxiliaryData = (txAuxiliaryData: TxAuxiliaryData): any => {
     switch (txAuxiliaryData.type) {
       case 'CATALYST_VOTING':
         return {
@@ -318,7 +318,7 @@ const ShelleyTrezorCryptoProvider = async ({
             stakingPath: txAuxiliaryData.rewardDestinationAddress.stakingPath,
             rewardAddressParameters: {
               addressType: AddressTypes.REWARD,
-              path: txAuxiliaryData.rewardDestinationAddress.stakingPath, //TODO: del, avoids error
+              path: txAuxiliaryData.rewardDestinationAddress.stakingPath, //TODO: required now
               stakingPath: txAuxiliaryData.rewardDestinationAddress.stakingPath,
             },
             nonce: `${txAuxiliaryData.nonce}`,
@@ -375,7 +375,7 @@ const ShelleyTrezorCryptoProvider = async ({
       ? `${txAux.validityIntervalStart}`
       : null
     const formattedAuxiliaryData = txAux.auxiliaryData
-      ? formatAuxiliaryAdata(txAux.auxiliaryData)
+      ? formatAuxiliaryData(txAux.auxiliaryData)
       : null
     const response: TrezorSignTxResponse = await TrezorConnect.cardanoSignTransaction(
       removeNullFields({
